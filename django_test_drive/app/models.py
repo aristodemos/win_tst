8c56f980417d39ce6bb6ba699c10219e28ba5db1
unknown <a.rpiko@gmail.com>
2014-10-21 20:41:58 +0300

    ride on

--- django_test_drive/app/models.py
+++ django_test_drive/app/models.py
@@ -5,8 +5,10 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
-def question (models.Model):
-    qn_verbose = models.CharField(max_length=300)
-    right_ans = models.ForeignKey('answers')
+class question (models.Model):
+    qn_verb     = models.CharField(max_length=300)
+    right_ans   = models.ForeignKey('answer')
 
-
+class answer (models.Model):
+    ans_verb    = models.CharField(max_length=300)
+    qn          = models.ForeignKey(question)
